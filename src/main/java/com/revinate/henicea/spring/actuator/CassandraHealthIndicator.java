2122c8bd329e5a5772446972d10dd757eb463eee
Jorge Lee <jrg.lee@gmail.com>
2015-11-02 12:12:05 -0800

    Add health indicator from Letter

---
+++ src/main/java/com/revinate/henicea/spring/actuator/CassandraHealthIndicator.java
@@ -0,0 +1,39 @@
+package com.revinate.henicea.spring.actuator;
+
+import com.datastax.driver.core.Host;
+import com.datastax.driver.core.Session;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.Status;
+
+import java.util.Map;
+
+import static java.util.stream.Collectors.toMap;
+
+@Slf4j
+public class CassandraHealthIndicator extends AbstractHealthIndicator {
+
+    @Autowired
+    private Session session;
+
+    @Override
+    protected void doHealthCheck(Health.Builder builder) throws Exception {
+        int openConnections = countOpenConnections();
+
+        builder
+                .status(openConnections == 0 ? Status.DOWN : Status.UP)
+                .withDetail("servers", getHostStatus())
+                .withDetail("openConnections", openConnections);
+    }
+
+    private Integer countOpenConnections() {
+        return session.getCluster().getMetrics().getOpenConnections().getValue();
+    }
+
+    private Map<String, String> getHostStatus() {
+        return session.getState().getConnectedHosts().stream()
+                .collect(toMap(host -> host.getAddress().getHostName(), Host::getState));
+    }
+}
---
+++ src/test/java/com/revinate/henicea/spring/actuator/CassandraHealthIndicatorTest.java
@@ -0,0 +1,67 @@
+package com.revinate.henicea.spring.actuator;
+
+import com.datastax.driver.core.Host;
+import com.datastax.driver.core.Session;
+import com.google.common.collect.ImmutableMap;
+import org.assertj.core.api.StrictAssertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Answers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.Status;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.data.MapEntry.entry;
+import static org.assertj.core.util.Lists.newArrayList;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CassandraHealthIndicatorTest {
+
+    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
+    Session session;
+
+    @InjectMocks
+    CassandraHealthIndicator indicator;
+
+    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
+    Host host;
+
+    @Before
+    public void setUp() throws Exception {
+        when(session.getState().getConnectedHosts()).thenReturn(newArrayList(host));
+        when(host.getAddress().getHostName()).thenReturn("server");
+    }
+
+    @Test
+    public void health_shouldBeUp_whenThereAreAvailableConnections() throws Exception {
+        when(session.getCluster().getMetrics().getOpenConnections().getValue()).thenReturn(2);
+        when(host.getState()).thenReturn("UP");
+
+        Health health = indicator.health();
+
+        StrictAssertions.assertThat(health).isNotNull();
+        StrictAssertions.assertThat(health.getStatus()).isEqualTo(Status.UP);
+        assertThat(health.getDetails())
+                .contains(entry("openConnections", 2))
+                .contains(entry("servers", ImmutableMap.of("server", "UP")));
+    }
+
+    @Test
+    public void health_shouldBeDown_whenThereAreNoneAvailableConnections() throws Exception {
+        when(session.getCluster().getMetrics().getOpenConnections().getValue()).thenReturn(0);
+        when(host.getState()).thenReturn("DOWN");
+
+        Health health = indicator.health();
+
+        StrictAssertions.assertThat(health).isNotNull();
+        StrictAssertions.assertThat(health.getStatus()).isEqualTo(Status.DOWN);
+        assertThat(health.getDetails())
+                .contains(entry("openConnections", 0))
+                .contains(entry("servers", ImmutableMap.of("server", "DOWN")));
+    }
+}
